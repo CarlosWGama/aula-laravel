4cab3ef11bde52f1acc7bf25598ea9c6c0c406dc
Carlos W. Gama <carloswgama@gmail.com>
2021-02-15 13:32:49 -0300

    View parte I

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller {
     //
-    public function login() { }
+    public function login() {
+        return view('login');
+    }
+
+    public function logar() {
+        echo 'Realizar login';
+    }
+
+    public function logout() {
+        return view('login');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', 'LoginController@login')->name('login');
 Route::get('/login', 'LoginController@login');
-
+Route::post('/logar', 'LoginController@logar')->name('logar');
+Route::get('/logout', 'LoginController@logout');
 
 Route::group(['prefix' => 'livros'], function () {
     Route::get('/', 'LivrosController@listar')->name('livros.listar');
-    Route::get('/novo',  'LivrosController@novo')->name('livros.novo');
-    Route::get('/editar/{id}',  'LivrosController@editar')->name('livros.editar');
-    Route::get('/visualizar/{id}',  'LivrosController@visualizar')->name('livros.visualizar');
+    Route::get('/novo', 'LivrosController@novo')->name('livros.novo');
+    Route::get('/editar/{id}', 'LivrosController@editar')->name('livros.editar');
+    Route::get('/excluir/{id}', 'LivrosController@excluir')->name('livros.excluir');
+    Route::get('/visualizar/{id}', 'LivrosController@visualizar')->name('livros.visualizar');
 });
